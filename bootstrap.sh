8c3946f902756baa790aea2642a0cd880e414cf7
yuya yano <yuya.yano.6260@gmail.com>
2014-10-15 21:35:22 +0900

    Added mercurial config.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-DOT_FILES=( .zshrc .zshrc.env .gitconfig .screenrc .vimrc .vim .tmux.conf .tmux-powerline)
+DOT_FILES=( .zshrc .zshrc.env .gitconfig .screenrc .vimrc .vim .tmux.conf .tmux-powerline .hgrc)
 
 for file in ${DOT_FILES[@]}
 do
@@ -7,6 +7,18 @@ done
 
 # Neobundle
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins -> vim -c ':NeoBundleInstall'"
+[ ! -d ~/.vim/bundle/vimproc.vim ] && git clone git@github.com:Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
+
+case "${OSTYPE}" in
+# Mac(Unix)
+darwin*)0
+    cd ~/.vim/bundle/vimproc.vim && make -f make_mac.mak
+    ;;  
+# Linux
+linux*)
+    cd ~/.vim/bundle/vimproc.vim && make -f make_unix.mak
+    ;;  
+esac
 
 function askYesOrNo {
   MSG=$1
@@ -23,6 +35,6 @@ function askYesOrNo {
 
 askYesOrNo ">>> GUI?"
 if [ $? -eq 0 ]; then
-	ls -s -i $HOME/dotfiles/.vimperatorrc $HOME/.vimperatorrc
-	ls -s -i $HOME/dotfiles/.vimperator $HOME/.vimperator
+	ln -s -i $HOME/dotfiles/.vimperatorrc $HOME/.vimperatorrc
+	ln -s -i $HOME/dotfiles/.vimperator $HOME/.vimperator
 fi
