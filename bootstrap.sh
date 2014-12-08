6203190d145eba010c83f5c3368285054e9ade92
yuya yano <yuya.yano.6260@gmail.com>
2014-12-08 02:46:47 +0000

    Fixed bug.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-DOT_FILES=( .zshrc .zshrc.env .gitconfig .screenrc .vimrc .vim .tmux.conf .tmux-powerline .hgrc)
+DOT_FILES=( .zshrc .zshrc.env .gitconfig .screenrc .vimrc .vim .tmux.conf .tmux-powerline .hgrc )
 
 for file in ${DOT_FILES[@]}
 do
