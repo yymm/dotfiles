845f2ab6485da3b09905dba3f9f5d589f9862efa
yymm <yuya.yano.6260@gmail.com>
2016-01-13 11:55:52 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,22 +19,3 @@ linux*)
     cd ~/.vim/bundle/vimproc.vim && make -f make_unix.mak
     ;;  
 esac
-
-function askYesOrNo {
-  MSG=$1
-  while :
-  do
-    echo -n "${MSG} y/N: "
-    read ans
-    case $ans in
-    [yY]) return 0 ;;
-    [nN]) return 1 ;;
-    esac
-  done
-}
-
-askYesOrNo ">>> GUI?"
-if [ $? -eq 0 ]; then
-	ln -s -i $HOME/dotfiles/.vimperatorrc $HOME/.vimperatorrc
-	ln -s -i $HOME/dotfiles/.vimperator $HOME/.vimperator
-fi
