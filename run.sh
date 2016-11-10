9b27dbbd5025dda17f8601dffa14d5d86e6423b9
yuya yano <yuya.yano.6260@gmail.com>
2016-11-10 16:04:40 +0900

    remove run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-DOT_FILES=( .zshrc .zshrc.env .gitconfig .screenrc .vimrc .vim .tmux.conf .tmux-powerline .hgrc )
-
-for file in ${DOT_FILES[@]}
-do
-	ln -nfs -i $HOME/.dotfiles/$file $HOME/$file
-done
